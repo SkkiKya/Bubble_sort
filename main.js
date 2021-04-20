51e919f51f991ed738f5379c3c1e3666f4d5a332
SkkiKya <koya.sakaki@gmail.com>
2021-04-20 23:04:49 +0900

    Bubble Sort

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
             for(var j=0; j<arrays.length - i - 1; j++){
                 // exchange
                 if(arrays[j] > arrays[j+1]){
-                    var tmp = arrays[j];
+                    const tmp = arrays[j];
                     arrays[j] = arrays[j+1];
                     arrays[j+1] = tmp;
                     isSwap=true;
